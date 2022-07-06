f214f74a8ed5c98d237ccaf3d8a4173e9b6f4009
Dennizz <root@ipaddr.nl>
2022-07-06 13:08:48 +0200

    output

--- output.tf
+++ output.tf
@@ -1,17 +1,8 @@
-#Mockup map of all LB's lookup per region/cloud
+#Map of all LB's lookup per region/cloud
 output "private_mode_details" {
-  value = {
-    "eu-west-1" = {
-      private_mode_lb_vpc_id   = "vpc-12345678"
-      private_mode_subnet_zone = "eu-west-1a"
-    }
-    "eu-west-2" = {
-      private_mode_lb_vpc_id   = "vpc-12345678"
-      private_mode_subnet_zone = "eu-west-2a"
-    }
-    "West Europe" = {
-      private_mode_lb_vpc_id   = "vnet1:rg-1"
-      private_mode_subnet_zone = "West Europe"
+  value = { for k in keys(var.secondary_regions) : k => {
+    private_mode_lb_vpc_id   = aviatrix_vpc.default[k].vpc_id
+    private_mode_subnet_zone = "eu-west-2a" #Need to look into gathering this value
     }
   }
 }
